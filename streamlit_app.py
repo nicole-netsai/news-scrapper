63583365cbb3d02aebf6444ff4d30324e965a803
nicole-netsai <nharaundanicole@gmail.com>
2025-05-06 21:38:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-!pip install scikit-learn
+pip install scikit-learn
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
